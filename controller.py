0ca3d5dbe118f814e6da0212c93fecca721db49d
andsmile007 <113000844+andsmile007@users.noreply.github.com>
2023-05-22 20:29:14 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -3,14 +3,20 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.topology import event, switches
+import ryu.topology.api as topo
+
 from ryu.ofproto import ofproto_v1_0
-from ryu.lib.packet import packet, ethernet, ether_types, arp
+from ryu.lib.packet import packet, ethernet, ether_types, arp,icmp
 from ryu.lib.packet import dhcp
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import packet
 from ryu.lib.packet import udp
 from dhcp import DHCPServer
+
+from ofctl_utils import OfCtl, VLANID_NONE
+
+from topo_manager_example import TopoManager
 
 import networkx as nx
 
@@ -19,18 +25,36 @@ class ControllerApp(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(ControllerApp, self).__init__(*args, **kwargs)
+        
+        self.tm = TopoManager()
 
     @set_ev_cls(event.EventSwitchEnter)
     def handle_switch_add(self, ev):
         """
         Event handler indicating a switch has come online.
         """
+        switch = ev.switch
+
+        self.logger.warn("Added Switch switch%d with ports:", switch.dp.id)
+        for port in switch.ports:
+            self.logger.warn("\t%d:  %s", port.port_no, port.hw_addr)
+
+        # TODO:  Update network topology and flow rules
+        self.tm.add_switch(switch)
 
     @set_ev_cls(event.EventSwitchLeave)
     def handle_switch_delete(self, ev):
         """
         Event handler indicating a switch has been removed
         """
+        
+        switch = ev.switch
+
+        self.logger.warn("Removed Switch switch%d with ports:", switch.dp.id)
+        for port in switch.ports:
+            self.logger.warn("\t%d:  %s", port.port_no, port.hw_addr)
+
+        # TODO:  Update network topology and flow rules
 
 
     @set_ev_cls(event.EventHostAdd)
@@ -39,13 +63,27 @@ class ControllerApp(app_manager.RyuApp):
         Event handler indiciating a host has joined the network
         This handler is automatically triggered when a host sends an ARP response.
         """ 
+        
+        host = ev.host
+        self.logger.warn("Host Added:  %s (IPs:  %s) on switch%s/%s (%s)",
+                          host.mac, host.ipv4,
+                         host.port.dpid, host.port.port_no, host.port.hw_addr)
+
         # TODO:  Update network topology and flow rules
+        self.tm.add_host(host)
+       
 
     @set_ev_cls(event.EventLinkAdd)
     def handle_link_add(self, ev):
         """
         Event handler indicating a link between two switches has been added
         """
+        link = ev.link
+        src_port = ev.link.src
+        dst_port = ev.link.dst
+        self.logger.warn("Added Link:  switch%s/%s (%s) -> switch%s/%s (%s)",
+                         src_port.dpid, src_port.port_no, src_port.hw_addr,
+                         dst_port.dpid, dst_port.port_no, dst_port.hw_addr)
         # TODO:  Update network topology and flow rules
 
     @set_ev_cls(event.EventLinkDelete)
@@ -53,6 +91,13 @@ class ControllerApp(app_manager.RyuApp):
         """
         Event handler indicating when a link between two switches has been deleted
         """
+        link = ev.link
+        src_port = link.src
+        dst_port = link.dst
+
+        self.logger.warn("Deleted Link:  switch%s/%s (%s) -> switch%s/%s (%s)",
+                          src_port.dpid, src_port.port_no, src_port.hw_addr,
+                          dst_port.dpid, dst_port.port_no, dst_port.hw_addr)
         # TODO:  Update network topology and flow rules
    
         
@@ -63,6 +108,11 @@ class ControllerApp(app_manager.RyuApp):
         Event handler for when any switch port changes state.
         This includes links for hosts as well as links between switches.
         """
+        port = ev.port
+        self.logger.warn("Port Changed:  switch%s/%s (%s):  %s",
+                         port.dpid, port.port_no, port.hw_addr,
+                         "UP" if port.is_live() else "DOWN")
+
         # TODO:  Update network topology and flow rules
 
 
@@ -77,6 +127,13 @@ class ControllerApp(app_manager.RyuApp):
             inPort = msg.in_port
             if not pkt_dhcp:
                 # TODO: handle other protocols like ARP 
+                if eth.ethertype == ether_types.ETH_TYPE_ARP:
+                    arp_msg = pkt.get_protocols(arp.arp)[0]
+
+                    if arp_msg.opcode == arp.ARP_REQUEST:
+
+                        self.logger.warning("Received ARP REQUEST on switch%d/%d:  Who has %s?  Tell %s",
+                                            dp.id, in_port, arp_msg.dst_ip, arp_msg.src_mac)             
                 pass
             else:
                 DHCPServer.handle_dhcp(datapath, inPort, pkt)      
