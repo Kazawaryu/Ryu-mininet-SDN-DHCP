648189827840153cfabd2024c91dbb48ce71beb0
alundyz <88989587+alundyz@users.noreply.github.com>
2023-05-29 22:45:42 +0800

    Update topo_manager.py

--- topo_manager.py
+++ topo_manager.py
@@ -17,7 +17,6 @@ class Device():
     Any device (switch or host) has a name (used for debugging only)
     and a set of neighbors.
     """
-
     def __init__(self, name):
         self.name = name
         self.neighbors = set()
@@ -90,7 +89,6 @@ class TopoManager():
     """
     Example class for keeping track of the network topology
     """
-
     def __init__(self):
         # TODO:  Initialize some data structures
         self.all_devices = []
@@ -98,35 +96,38 @@ class TopoManager():
         self.switches_dev = {}
         self.hosts = []
         self.links = {}
-
+        
         self.node_port = {}
 
     def add_link(self, link):
         if link.src.dpid not in self.links.keys():
-            self.links[link.src.dpid] = [(link.dst, link.src, 1)]
+            self.links[link.src.dpid] = [ (link.dst, link.src, 1) ]
         else:
-            self.links[link.src.dpid].append((link.dst, link.src, 1))
+            self.links[link.src.dpid].append( (link.dst, link.src, 1) )
 
         # if link.dst.dpid not in self.links.keys():
         #     self.links[link.dst.dpid] = [ (link.src, link.dst, 1) ]
         # else:
         #     self.links[link.dst.dpid].append ( (link.src, link.dst, 1) )
 
+        
+  
     def delete_link(self, link):
         if link.src.dpid in self.links.keys():
-            self.links[link.src.dpid].remove((link.dst, link.src, 1))
+            self.links[link.src.dpid].remove( (link.dst, link.src, 1) )
         # if link.dst.dpid in self.links.keys():
         #     self.links[link.dst.dpid].remove( (link.src, link.dst, 1) )
 
+        
+    
     def add_switch(self, sw):
         name = "switch_{}".format(sw.dp.id)
         switch = TMSwitch(name, sw)
         self.switches.append(switch)
         self.all_devices.append(switch)
-        self.node_port[sw.dp.id] = set()
+        self.node_port[sw.dp.id]=set()
 
         # TODO:  Add switch to some data structure(s)
-
     def delete_switch(self, sw):
         for s in self.switches:
             if s.get_dpid() == sw.dp.id:
@@ -134,11 +135,11 @@ class TopoManager():
                 self.all_devices.remove(s)
                 if s in self.switches_dev.keys():
                     del self.switches_dev[s]
-
+        
                 if s.get_dpid() in self.links.keys():
                     del self.links[s.get_dpid()]
                 break
-        del (self.node_port[sw])
+        del(self.node_port[sw]) 
 
     def add_host(self, h):
         name = "host_{}".format(h.mac)
