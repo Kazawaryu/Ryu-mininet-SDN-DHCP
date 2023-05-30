97363d3b8e36e36948b5a2e9860b63eb6d5feb47
alundyz <88989587+alundyz@users.noreply.github.com>
2023-05-31 00:09:03 +0800

    Update test_network.py

--- tests/switching_test/test_network.py
+++ tests/switching_test/test_network.py
@@ -21,6 +21,9 @@ def send_dhcp(node):
     print('Sending DHCP request dhclient -v %s-eth0 '% (node.name))
     node.cmd('dhclient -v %s-eth0' % (node.name))
 
+def pingall(node):
+    node.cmd('pingall')
+
 
 def do_arp_all(net):
     for h in net.hosts:
@@ -43,9 +46,62 @@ class TriangleTopo(Topo):
         self.addLink(s3, s1)
 
 
+class TestTopo(Topo):
+    def __init__(self, **opts):
+        Topo.__init__(self, **opts)
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+        h7 = self.addHost('h7')
+
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+        s5 = self.addSwitch('s5')
+        s6 = self.addSwitch('s6')
+        s7 = self.addSwitch('s7')
+        s8 = self.addSwitch('s8')
+        s9 = self.addSwitch('s9')
+        s10 = self.addSwitch('s10')
+
+        self.addLink(h1, s1)
+        self.addLink(h2, s2)
+        self.addLink(h3, s3)
+        self.addLink(h4, s4)
+        self.addLink(h5, s5)
+        self.addLink(h6, s6)
+        self.addLink(h7, s7)
+
+        self.addLink(s1,s8)
+        self.addLink(s8,s2)
+        self.addLink(s1,s9)
+
+
+        self.addLink(s10,s2)
+
+        self.addLink(s9,s10)
+
+        self.addLink(s9,s5)
+        self.addLink(s9,s6)
+        self.addLink(s9,s7)
+
+
+        self.addLink(s10,s5)
+        self.addLink(s10,s4)
+        self.addLink(s10,s3)
+
+
+        
+        
+
 def run_mininet():
     import time
-    topo = TriangleTopo()
+    # topo = TriangleTopo()
+    topo = TestTopo()
     net = Mininet(topo=topo, autoSetMacs=True, controller=RemoteController)
     for h in net.hosts:
         disable_ipv6(h)
